786911e739f9e16e9b3ede5cfa888197db5d75b5
BarWieleba <barwieleba@gmail.com>
2022-06-04 19:12:27 +0200

    invoker

--- src/main/java/com/primenumbers/invokers/OwnedVehicleInvoker.java
+++ src/main/java/com/primenumbers/invokers/OwnedVehicleInvoker.java
@@ -10,24 +10,57 @@ public class OwnedVehicleInvoker {
 
     }
 
+    public void saveOwnedVehicles100() {
+        ownedVehicleTest(100);
+    }
 
-    public void ownedVehicle100(){
+    public void saveOwnedVehicles1000() {
+        ownedVehicleTest(1000);
+    }
+
+    public void saveOwnedVehicles10000() {
+        ownedVehicleTest(10000);
+    }
+
+
+    private void ownedVehicleTest(int amount){
         OwnedVehicleController ownedVehicleController = new OwnedVehicleController();
         ownedVehicleController.delete(); //by najpierw wyczyscic tabelę
 
-        System.out.println("Save OwnedVehicle 100 Times: ");
+        System.out.println("Save OwnedVehicle " + amount +" Times: ");
         List<Long> savingTimes = new ArrayList<>();
         List<Long> updateTimes = new ArrayList<>();
         List<Long> deletingTimes = new ArrayList<>();
 
         for(int i = 0; i<50; i++) {
 
+            long saveStart = System.currentTimeMillis();
+            ownedVehicleController.save(amount);
+            long saveEnd = System.currentTimeMillis();
+            savingTimes.add(saveEnd-saveStart);
 
-            ownedVehicleController.save(100);
+            long updateStart = System.currentTimeMillis();
+            ownedVehicleController.update(amount);
+            long updateEnd = System.currentTimeMillis();
+            updateTimes.add(updateEnd-updateStart);
 
-            ownedVehicleController.update(100);
-
+            long deleteStart = System.currentTimeMillis();
             ownedVehicleController.delete();
+            long deleteEnd = System.currentTimeMillis();
+            deletingTimes.add(deleteEnd-deleteStart);
+        }
+
+        System.out.println("SaveTimes: ");
+        for(long time : savingTimes) {
+            System.out.println(time);
+        }
+        System.out.println("UpdateTimess: ");
+        for(long time : updateTimes) {
+            System.out.println(time);
+        }
+        System.out.println("DeleteTimes: ");
+        for(long time : deletingTimes) {
+            System.out.println(time);
         }
 
     }
